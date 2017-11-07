1185430b306c9e9455d613ecefd73f3e7d21c0fc
Your Name HereSajjad <sajjad.taghilou@outlook.com>
2017-11-07 21:45:41 +0200

    add

--- Model.js
+++ Model.js
@@ -18,4 +18,40 @@ x ("amir","ali","yadi");
  	var c= a + b;
  	return c;
  }
- document.write(addnumbers(3,6));
+ document.write(addnumbers(3,6)+"</br>");
+
+ var first = "adel"
+ var last="mirzai"
+
+
+ if((first=="adel") || (last=="mirzai")){
+ 	document.write ("salam iran"+ "</br>")
+ }
+  var girl = "sara"
+ switch (girl) {
+ 	case "sara":
+ 	document.write("heloo sara"+ "</br>")
+ 	break;
+ 	case "neda":
+ 	document.write("heloo neda"+ "</br>")
+ 	break;
+ 	default:
+ 	document.write("heloo sajjad");
+ }
+
+
+for (var i = 0; i < 10; i++) {
+	document.write("javascript" + "<br/>")
+}
+
+var x=1
+while(x<10){
+	document.write (x+"<br/>")
+	x++;
+}
+
+var y=20
+do{
+	document.write(y +"</br>")
+	y++;
+}while (y<25);
